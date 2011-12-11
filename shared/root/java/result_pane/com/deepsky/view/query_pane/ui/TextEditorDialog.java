7a20f6880fb40c36103852a2daf007b10e6447de
unknown <sky@.(none)>
2011-12-11 10:49:39 +0200

    Clean up import list

--- shared/root/java/result_pane/com/deepsky/view/query_pane/ui/TextEditorDialog.java
+++ shared/root/java/result_pane/com/deepsky/view/query_pane/ui/TextEditorDialog.java
@@ -34,10 +34,8 @@ import com.intellij.openapi.fileChooser.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileWrapper;
-import org.codehaus.groovy.util.StringUtil;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
@@ -46,11 +44,8 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.sql.SQLException;
 
 public class TextEditorDialog extends DialogWrapper {
