13ae34da4b7fffdd2b13d9a5cca5668503a9b135
Serhiy Kulyk <plsql.assistant@gmail>
2014-03-15 09:28:16 +0200

    Remove redundant files and add test files

---
+++ db_browser/src/test/resources/completion/base/select/inner_join2.sql
@@ -0,0 +1,17 @@
+CREATE TABLE suppliers
+( supplier_id number(10) not null,
+  supplier_name varchar2(50) not null,
+  contact_name varchar2(50),
+    CONSTRAINT suppliers_pk PRIMARY KEY (supplier_id)
+);
+
+CREATE TABLE orders
+( supplier_id number(10) not null,
+  order_name varchar2(50) not null,
+  order_date date
+);
+
+SELECT suppliers.supplier_id, suppliers.supplier_name, orders.order_date
+FROM suppliers
+    INNER JOIN <caret>
+    ON suppliers.supplier_id = orders.supplier_id;
